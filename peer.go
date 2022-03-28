a69b4b683988b4b01d4b7d66364bd103d8b0c68f
Malte Janduda <malte.janduda@sap.com>
2022-03-28 16:09:34 +0200

    refactoring

--- peer.go
+++ peer.go
@@ -7,24 +7,31 @@ import (
 	"sync"
 	"time"
 
-	"github.com/onmetal/metalbond/pb"
 	"github.com/sirupsen/logrus"
-	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 type MetalBondPeer struct {
-	conn                net.Conn
-	remoteAddr          string
-	txChan              chan []byte
-	direction           ConnectionDirection
-	state               ConnectionState
-	stateLock           sync.RWMutex
-	database            *MetalBondDatabase
-	keepaliveInterval   uint32
-	keepaliveTimer      *time.Timer
+	conn       net.Conn
+	remoteAddr string
+	direction  ConnectionDirection
+
+	state     ConnectionState
+	stateLock sync.RWMutex
+
+	database *MetalBondDatabase
+
+	keepaliveInterval uint32
+	keepaliveTimer    *time.Timer
+
 	mySubscriptions     map[uint32]bool
 	mySubscriptionsLock sync.Mutex
+
+	txChan        chan []byte
+	rxHello       chan msgHello
+	rxKeepalive   chan msgKeepalive
+	rxSubscribe   chan msgSubscribe
+	rxUnsubscribe chan msgUnsubscribe
+	rxUpdate      chan msgUpdate
 }
 
 func NewMetalBondPeer(
@@ -58,7 +65,7 @@ func NewMetalBondPeer(
 		database:          database,
 	}
 
-	go peer.Handle()
+	go peer.handle()
 
 	return &peer
 }
@@ -69,6 +76,56 @@ func (p *MetalBondPeer) GetState() ConnectionState {
 	p.stateLock.RUnlock()
 	return state
 }
+
+func (p *MetalBondPeer) Subscribe(vni uint32) error {
+	if p.direction == INCOMING {
+		return fmt.Errorf("Cannot subscribe on incoming connection")
+	}
+
+	p.mySubscriptionsLock.Lock()
+	defer p.mySubscriptionsLock.Unlock()
+	if _, exists := p.mySubscriptions[vni]; exists {
+		return fmt.Errorf("Already subscribed")
+	}
+
+	p.mySubscriptions[vni] = true
+
+	msg := msgSubscribe{
+		VNI: vni,
+	}
+
+	return p.sendMessage(msg)
+}
+
+func (p *MetalBondPeer) Unsubscribe(vni uint32) error {
+	if p.direction == INCOMING {
+		return fmt.Errorf("Cannot unsubscribe on incoming connection")
+	}
+
+	p.mySubscriptionsLock.Lock()
+	defer p.mySubscriptionsLock.Unlock()
+	if _, exists := p.mySubscriptions[vni]; !exists {
+		return fmt.Errorf("Not subscribed to table %d", vni)
+	}
+
+	delete(p.mySubscriptions, vni)
+
+	msg := msgUnsubscribe{
+		VNI: vni,
+	}
+
+	return p.sendMessage(msg)
+}
+
+func (p *MetalBondPeer) SendUpdate(r RouteUpdate) error {
+	msg := []byte{}
+	p.txChan <- msg
+	return nil
+}
+
+///////////////////////////////////////////////////////////////////
+//            PRIVATE METHODS BELOW                              //
+///////////////////////////////////////////////////////////////////
 
 func (p *MetalBondPeer) setState(state ConnectionState) {
 	p.stateLock.Lock()
@@ -96,97 +153,50 @@ func (p *MetalBondPeer) log() *logrus.Entry {
 	return logrus.WithField("peer", p.remoteAddr).WithField("state", state)
 }
 
-func (p *MetalBondPeer) Handle() {
+func (p *MetalBondPeer) handle() {
 	p.txChan = make(chan []byte)
+	p.rxHello = make(chan msgHello)
+	p.rxKeepalive = make(chan msgKeepalive)
+	p.rxSubscribe = make(chan msgSubscribe)
+	p.rxUnsubscribe = make(chan msgUnsubscribe)
+	p.rxUpdate = make(chan msgUpdate)
 	go p.rxLoop()
 	go p.txLoop()
 
 	if p.direction == OUTGOING {
-		helloMsg := pb.Hello{
+		helloMsg := msgHello{
 			KeepaliveInterval: p.keepaliveInterval,
 		}
 
-		p.sendMessage(HELLO, &helloMsg)
+		p.sendMessage(helloMsg)
 		p.setState(HELLO_SENT)
 	}
 
 	p.log().Infof("Peer connected")
-}
 
-func (p *MetalBondPeer) Close() {
-	if p.GetState() == CLOSED {
-		p.log().Errorf("Connection Close() called twice.")
-		return
-	}
-
-	p.log().Infof("Closing peer connection")
-	p.setState(CLOSED)
-
-	// Sending zero length msg to txchan to indicate connection termination
-	// txLoop will close connection, rxLoop will notice that.
-	p.txChan <- []byte{}
-
-	// If the connection was OUTGOING, try to reconnect!
-	if p.direction == OUTGOING {
-		p.log().Infof("Trying to reconnect...")
-
-		// TODO: Potential memory leak. This memory address is maintained nowhere. It's a zombie thread, doing stuff.
-		NewMetalBondPeer(nil, p.remoteAddr, p.keepaliveInterval, OUTGOING, p.database)
-	}
-}
-
-func (p *MetalBondPeer) txLoop() {
 	for {
-		msg := <-p.txChan
+		select {
+		case msg := <-p.rxHello:
+			p.log().Debugf("Received HELLO message")
+			p.processRxHello(msg)
 
-		// msg length is zero when peer connection should be terminated
-		if len(msg) == 0 {
-			p.conn.Close()
-			return
-		}
+		case msg := <-p.rxKeepalive:
+			p.log().Debugf("Received KEEPALIVE message")
+			p.processRxKeepalive(msg)
 
-		n, err := p.conn.Write(msg)
-		if n != len(msg) || err != nil {
-			p.log().Errorf("Could not transmit message completely: %v", err)
-			p.Close()
-		}
-	}
-}
+		case msg := <-p.rxSubscribe:
+			p.log().Debugf("Received SUBSCRIBE message")
+			p.processRxSubscribe(msg)
 
-func (p *MetalBondPeer) startKeepaliveTimer() {
-	timeout := time.Duration(p.keepaliveInterval) * time.Second * 5 / 2
-	p.log().Infof("Timeout duration: %v", timeout)
-	p.keepaliveTimer = time.NewTimer(timeout)
-	go func() {
-		<-p.keepaliveTimer.C
-		if p.GetState() != CLOSED {
-			p.log().Infof("Connection timed out. Closing.")
-			p.Close()
-		}
-	}()
+		case msg := <-p.rxUnsubscribe:
+			p.log().Debugf("Received UNSUBSCRIBE message")
+			p.processRxUnsubscribe(msg)
 
-	if p.direction == OUTGOING {
-		interval := time.Duration(p.keepaliveInterval) * time.Second
-		p.log().Infof("Starting KEEPALIVE interval sender (%v)", interval)
-		for {
-			state := p.GetState()
-			switch state {
-			case HELLO_RECEIVED:
-				p.sendMessage(KEEPALIVE, nil)
-			case ESTABLISHED:
-				p.sendMessage(KEEPALIVE, nil)
-			default:
-				p.log().Debugf("Shutting down keepalive timer as peer is in wrong state (%v).", state)
-				return
-			}
-
-			time.Sleep(interval)
+		case msg := <-p.rxUpdate:
+			p.log().Debugf("Received UPDATE message")
+			p.processRxUpdate(msg)
 		}
 	}
-}
-
-func (p *MetalBondPeer) resetKeepaliveTimeout() {
-	p.keepaliveTimer.Reset(time.Duration(p.keepaliveInterval) * time.Second * 5 / 2)
 }
 
 func (p *MetalBondPeer) rxLoop() {
@@ -226,88 +236,46 @@ func (p *MetalBondPeer) rxLoop() {
 
 			switch pktType {
 			case HELLO:
-				hello := &pb.Hello{}
-				if err := proto.Unmarshal(pktBytes, hello); err != nil {
-					p.log().Errorf("Cannot unmarshal received packet. Closing connection: %v", err)
+				hello, err := DeserializeHelloMsg(pktBytes)
+				if err != nil {
+					p.log().Errorf("Cannot deserialize HELLO message: %v", err)
+					p.Close()
 					return
 				}
 
-				// Use lower Keepalive interval of both client and server as peer config
-				keepaliveInterval := p.keepaliveInterval
-				if hello.KeepaliveInterval < keepaliveInterval {
-					keepaliveInterval = hello.KeepaliveInterval
-				}
-				p.keepaliveInterval = keepaliveInterval
-
-				// state transition to HELLO_RECEIVED
-				p.setState(HELLO_RECEIVED)
-				p.log().Infof("HELLO message received")
-
-				// if direction incoming, send HELLO response
-				if p.direction == INCOMING {
-					helloMsg := pb.Hello{
-						KeepaliveInterval: p.keepaliveInterval,
-					}
-
-					p.sendMessage(HELLO, &helloMsg)
-					p.setState(HELLO_SENT)
-					p.log().Infof("HELLO message sent")
-				}
-
-				// start keepalive timer so keepalive messages are sent by the client
-				// and client and server check if regular keepalive messages are received.
-				go p.startKeepaliveTimer()
+				p.rxHello <- *hello
 
 			case KEEPALIVE:
-				p.log().Debugf("KEEPALIVE message received")
+				p.rxKeepalive <- msgKeepalive{}
 
-				if p.direction == INCOMING && p.GetState() == HELLO_SENT {
-					p.setState(ESTABLISHED)
-				} else if p.direction == OUTGOING && p.GetState() == HELLO_RECEIVED {
-					p.setState(ESTABLISHED)
-				} else if p.GetState() == ESTABLISHED {
-					// all good
-				} else {
-					p.log().Errorf("Received KEEPALIVE while in wrong state. Closing connection.")
-					p.Close()
-				}
-
-				p.resetKeepaliveTimeout()
-
-				// The server must respond incoming KEEPALIVE messages with an own KEEPALIVE message.
-				if p.direction == INCOMING {
-					p.sendMessage(KEEPALIVE, nil)
-				}
-
-			// TODO: implement received SUBSCRIBE message
 			case SUBSCRIBE:
-				p.log().Infof("SUBSCRIBE message received")
-				msg := &pb.Subscription{}
-				if err := proto.Unmarshal(pktBytes, msg); err != nil {
-					p.log().Errorf("Cannot unmarshal received packet. Closing connection: %v", err)
+				sub, err := DeserializeSubscribeMsg(pktBytes)
+				if err != nil {
+					p.log().Errorf("Cannot deserialize SUBSCRIBE message: %v", err)
+					p.Close()
 					return
 				}
-				p.log().Infof("Content: %v", msg)
 
-			// TODO: implement received UNSUBSCRIBE message
+				p.rxSubscribe <- *sub
+
 			case UNSUBSCRIBE:
-				p.log().Infof("UNSUBSCRIBE message received")
-				msg := &pb.Subscription{}
-				if err := proto.Unmarshal(pktBytes, msg); err != nil {
-					p.log().Errorf("Cannot unmarshal received packet. Closing connection: %v", err)
+				sub, err := DeserializeUnsubscribeMsg(pktBytes)
+				if err != nil {
+					p.log().Errorf("Cannot deserialize UNSUBSCRIBE message: %v", err)
+					p.Close()
 					return
 				}
-				p.log().Infof("Content: %v", msg)
 
-			// TODO: implement received UPDATE message
+				p.rxUnsubscribe <- *sub
+
 			case UPDATE:
-				p.log().Infof("UPDATE message received")
-				msg := &pb.Update{}
-				if err := proto.Unmarshal(pktBytes, msg); err != nil {
-					p.log().Errorf("Cannot unmarshal received packet. Closing connection: %v", err)
+				upd, err := DeserializeUpdateMsg(pktBytes)
+				if err != nil {
+					p.log().Errorf("Cannot deserialize UPDATE message: %v", err)
+					p.Close()
 					return
 				}
-				p.log().Infof("Content: %v", msg)
+				p.rxUpdate <- *upd
 
 			default:
 				p.log().Errorf("Unknown Packet received. Closing connection.")
@@ -325,18 +293,146 @@ func (p *MetalBondPeer) rxLoop() {
 	}
 }
 
-func (p *MetalBondPeer) sendMessage(msgType MESSAGE_TYPE, msg protoreflect.ProtoMessage) error {
-	msgBytes := []byte{}
-	var err error
-	if msg != nil {
-		msgBytes, err = proto.Marshal(msg)
-		if err != nil {
-			return fmt.Errorf("Could not marshal message: %v", err)
+func (p *MetalBondPeer) processRxHello(msg msgHello) {
+	// Use lower Keepalive interval of both client and server as peer config
+	keepaliveInterval := p.keepaliveInterval
+	if msg.KeepaliveInterval < keepaliveInterval {
+		keepaliveInterval = msg.KeepaliveInterval
+	}
+	p.keepaliveInterval = keepaliveInterval
+
+	// state transition to HELLO_RECEIVED
+	p.setState(HELLO_RECEIVED)
+	p.log().Infof("HELLO message received")
+
+	// if direction incoming, send HELLO response
+	if p.direction == INCOMING {
+		helloMsg := msgHello{
+			KeepaliveInterval: p.keepaliveInterval,
 		}
+
+		p.sendMessage(helloMsg)
+		p.setState(HELLO_SENT)
+		p.log().Infof("HELLO message sent")
 	}
 
-	if len(msgBytes) > 1188 {
-		return fmt.Errorf("Message too long: %d bytes > maximum of 1188 bytes", len(msgBytes))
+	// start keepalive timer so keepalive messages are sent by the client
+	// and client and server check if regular keepalive messages are received.
+	go p.startKeepaliveTimer()
+}
+
+func (p *MetalBondPeer) processRxKeepalive(msg msgKeepalive) {
+	if p.direction == INCOMING && p.GetState() == HELLO_SENT {
+		p.setState(ESTABLISHED)
+	} else if p.direction == OUTGOING && p.GetState() == HELLO_RECEIVED {
+		p.setState(ESTABLISHED)
+	} else if p.GetState() == ESTABLISHED {
+		// all good
+	} else {
+		p.log().Errorf("Received KEEPALIVE while in wrong state. Closing connection.")
+		p.Close()
+	}
+
+	p.resetKeepaliveTimeout()
+
+	// The server must respond incoming KEEPALIVE messages with an own KEEPALIVE message.
+	if p.direction == INCOMING {
+		p.sendMessage(msgKeepalive{})
+	}
+}
+
+func (p *MetalBondPeer) processRxSubscribe(msg msgSubscribe) {
+}
+
+func (p *MetalBondPeer) processRxUnsubscribe(msg msgUnsubscribe) {
+}
+
+func (p *MetalBondPeer) processRxUpdate(msg msgUpdate) {
+}
+
+func (p *MetalBondPeer) Close() {
+	if p.GetState() == CLOSED {
+		p.log().Errorf("Connection Close() called twice.")
+		return
+	}
+
+	p.log().Infof("Closing peer connection")
+	p.setState(CLOSED)
+
+	// Sending zero length msg to txchan to indicate connection termination
+	// txLoop will close connection, rxLoop will notice that.
+	p.txChan <- []byte{}
+
+	// If the connection was OUTGOING, try to reconnect!
+	if p.direction == OUTGOING {
+		p.log().Infof("Trying to reconnect...")
+
+		// TODO: Potential memory leak. This memory address is maintained nowhere. It's a zombie thread, doing stuff.
+		NewMetalBondPeer(nil, p.remoteAddr, p.keepaliveInterval, OUTGOING, p.database)
+	}
+}
+
+func (p *MetalBondPeer) startKeepaliveTimer() {
+	timeout := time.Duration(p.keepaliveInterval) * time.Second * 5 / 2
+	p.log().Infof("Timeout duration: %v", timeout)
+	p.keepaliveTimer = time.NewTimer(timeout)
+	go func() {
+		<-p.keepaliveTimer.C
+		if p.GetState() != CLOSED {
+			p.log().Infof("Connection timed out. Closing.")
+			p.Close()
+		}
+	}()
+
+	if p.direction == OUTGOING {
+		interval := time.Duration(p.keepaliveInterval) * time.Second
+		p.log().Infof("Starting KEEPALIVE interval sender (%v)", interval)
+		for {
+			state := p.GetState()
+			switch state {
+			case HELLO_RECEIVED:
+				p.sendMessage(msgKeepalive{})
+			case ESTABLISHED:
+				p.sendMessage(msgKeepalive{})
+			default:
+				p.log().Debugf("Shutting down keepalive timer as peer is in wrong state (%v).", state)
+				return
+			}
+
+			time.Sleep(interval)
+		}
+	}
+}
+
+func (p *MetalBondPeer) resetKeepaliveTimeout() {
+	p.keepaliveTimer.Reset(time.Duration(p.keepaliveInterval) * time.Second * 5 / 2)
+}
+
+func (p *MetalBondPeer) sendMessage(msg message) error {
+	var msgType MESSAGE_TYPE
+	switch msg.(type) {
+	case msgHello:
+		msgType = HELLO
+		p.log().Debugf("Sending HELLO message")
+	case msgKeepalive:
+		msgType = KEEPALIVE
+		p.log().Debugf("Sending KEEPALIVE message")
+	case msgSubscribe:
+		msgType = SUBSCRIBE
+		p.log().Debugf("Sending SUBSCRIBE message")
+	case msgUnsubscribe:
+		msgType = UNSUBSCRIBE
+		p.log().Debugf("Sending UNSUBSCRIBE message")
+	case msgUpdate:
+		msgType = UPDATE
+		p.log().Debugf("Sending UPDATE message")
+	default:
+		return fmt.Errorf("Unknown message type")
+	}
+
+	msgBytes, err := msg.Serialize()
+	if err != nil {
+		return fmt.Errorf("Could not serialize message: %v", err)
 	}
 
 	hdr := []byte{1, byte(len(msgBytes) >> 8), byte(len(msgBytes) % 256), byte(msgType)}
@@ -347,101 +443,20 @@ func (p *MetalBondPeer) sendMessage(msgType MESSAGE_TYPE, msg protoreflect.Proto
 	return nil
 }
 
-func (p *MetalBondPeer) Subscribe(vni uint32) error {
-	if p.direction == INCOMING {
-		return fmt.Errorf("Cannot subscribe on incoming connection")
+func (p *MetalBondPeer) txLoop() {
+	for {
+		msg := <-p.txChan
+
+		// msg length is zero when peer connection should be terminated
+		if len(msg) == 0 {
+			p.conn.Close()
+			return
+		}
+
+		n, err := p.conn.Write(msg)
+		if n != len(msg) || err != nil {
+			p.log().Errorf("Could not transmit message completely: %v", err)
+			p.Close()
+		}
 	}
-
-	p.mySubscriptionsLock.Lock()
-	defer p.mySubscriptionsLock.Unlock()
-	if _, exists := p.mySubscriptions[vni]; exists {
-		return fmt.Errorf("Already subscribed")
-	}
-
-	p.mySubscriptions[vni] = true
-
-	msg := pb.Subscription{
-		Vni: vni,
-	}
-
-	return p.sendMessage(SUBSCRIBE, &msg)
-}
-
-func (p *MetalBondPeer) Unsubscribe(vni uint32) error {
-	if p.direction == INCOMING {
-		return fmt.Errorf("Cannot unsubscribe on incoming connection")
-	}
-
-	p.mySubscriptionsLock.Lock()
-	defer p.mySubscriptionsLock.Unlock()
-	if _, exists := p.mySubscriptions[vni]; !exists {
-		return fmt.Errorf("Not subscribed to table %d", vni)
-	}
-
-	delete(p.mySubscriptions, vni)
-
-	msg := pb.Subscription{
-		Vni: vni,
-	}
-
-	return p.sendMessage(UNSUBSCRIBE, &msg)
-}
-
-func (p *MetalBondPeer) SendUpdate(r RouteUpdate) error {
-	msg := []byte{}
-	p.txChan <- msg
-	return nil
-}
-
-func (p *MetalBondPeer) processReceivedUpdate(msg pb.Update) error {
-	var action UpdateAction
-	switch msg.Action {
-	case pb.Action_ADD:
-		action = ADD
-	case pb.Action_REMOVE:
-		action = REMOVE
-	default:
-		return fmt.Errorf("invalid update action")
-	}
-
-	var ipversion IPVersion
-	switch msg.Destination.IpVersion {
-	case pb.IPVersion_IPv4:
-		ipversion = IPV4
-	case pb.IPVersion_IPv6:
-		ipversion = IPV6
-	default:
-		return fmt.Errorf("invalid IP version")
-	}
-
-	if len(msg.Destination.Prefix) != 16 {
-		return fmt.Errorf("Invalid Prefix. Must be 16 bytes long! Also for IPv4!")
-	}
-	if len(msg.NextHop.TargetAddress) != 16 {
-		return fmt.Errorf("Invalid Prefix. Must be 16 bytes long! Also for IPv4!")
-	}
-
-	upd := RouteUpdate{
-		ReceivedFrom: p,
-		Action:       action,
-		VNI:          VNI(msg.Vni),
-		Destination: Destination{
-			IPVersion:    ipversion,
-			PrefixLength: uint8(msg.Destination.PrefixLength),
-		},
-		NextHop: NextHop{
-			TargetVNI:        msg.NextHop.TargetVNI,
-			NAT:              msg.NextHop.Nat,
-			NATPortRangeFrom: uint16(msg.NextHop.NatPortRangeFrom),
-			NATPortRangeTo:   uint16(msg.NextHop.NatPortRangeTo),
-		},
-	}
-	copy(upd.Destination.Prefix[:], msg.Destination.Prefix[:16])
-	copy(upd.NextHop.TargetAddress[:], msg.NextHop.TargetAddress[:16])
-
-	return p.database.Update(upd)
-}
-
-func (p *MetalBondPeer) ProcessProtoSubscribeMsg(msg pb.Update, receivedFrom *MetalBondPeer) error {
-	return nil
 }
--- types.go
+++ types.go
@@ -1,5 +1,13 @@
 package metalbond
 
+import (
+	"fmt"
+	"net"
+
+	"github.com/onmetal/metalbond/pb"
+	"google.golang.org/protobuf/proto"
+)
+
 /////////////////////////////////////////////////////////////
 //                           TYPES                         //
 /////////////////////////////////////////////////////////////
@@ -40,8 +48,8 @@ type RouteUpdate struct {
 type IPVersion uint8
 
 const (
-	IPV4 = 4
-	IPV6 = 6
+	IPV4 IPVersion = 4
+	IPV6 IPVersion = 6
 )
 
 type ConnectionDirection uint8
@@ -78,3 +86,152 @@ const (
 	ADD UpdateAction = iota
 	REMOVE
 )
+
+type message interface {
+	Serialize() ([]byte, error)
+}
+
+type msgHello struct {
+	message
+	KeepaliveInterval uint32
+}
+
+func (m msgHello) Serialize() ([]byte, error) {
+	pbmsg := pb.Hello{
+		KeepaliveInterval: m.KeepaliveInterval,
+	}
+
+	msgBytes, err := proto.Marshal(&pbmsg)
+	if err != nil {
+		return nil, fmt.Errorf("Could not marshal message: %v", err)
+	}
+
+	if len(msgBytes) > 1188 {
+		return nil, fmt.Errorf("Message too long: %d bytes > maximum of 1188 bytes", len(msgBytes))
+	}
+
+	return msgBytes, nil
+}
+
+type msgKeepalive struct {
+	message
+}
+
+func (msg msgKeepalive) Serialize() ([]byte, error) {
+	return []byte{}, nil
+}
+
+type msgSubscribe struct {
+	message
+	VNI uint32
+}
+
+func (msg msgSubscribe) Serialize() ([]byte, error) {
+	return []byte{}, nil
+}
+
+type msgUnsubscribe struct {
+	message
+	VNI uint32
+}
+
+func (msg msgUnsubscribe) Serialize() ([]byte, error) {
+	return []byte{}, nil
+}
+
+type msgUpdate struct {
+	message
+	Action      UpdateAction
+	VNI         uint32
+	Destination msgUpdateDestination
+	NextHop     msgUpdateNextHop
+}
+
+func (msg msgUpdate) Serialize() ([]byte, error) {
+	return []byte{}, nil
+}
+
+type msgUpdateDestination struct {
+	IPVersion    IPVersion
+	Prefix       net.IP
+	PrefixLength uint8
+}
+
+type msgUpdateNextHop struct {
+	TargetAddress    net.IP
+	TargetVNI        uint32
+	NAT              bool
+	NATPortRangeFrom uint16
+	NATPortRangeTo   uint16
+}
+
+func DeserializeHelloMsg(pktBytes []byte) (*msgHello, error) {
+	pbmsg := &pb.Hello{}
+	if err := proto.Unmarshal(pktBytes, pbmsg); err != nil {
+		return nil, fmt.Errorf("Cannot unmarshal received packet. Closing connection: %v", err)
+	}
+
+	return &msgHello{
+		KeepaliveInterval: pbmsg.KeepaliveInterval,
+	}, nil
+}
+
+func DeserializeSubscribeMsg(pktBytes []byte) (*msgSubscribe, error) {
+	pbmsg := &pb.Subscription{}
+	if err := proto.Unmarshal(pktBytes, pbmsg); err != nil {
+		return nil, fmt.Errorf("Cannot unmarshal received packet. Closing connection: %v", err)
+	}
+
+	return &msgSubscribe{
+		VNI: pbmsg.Vni,
+	}, nil
+}
+
+func DeserializeUnsubscribeMsg(pktBytes []byte) (*msgUnsubscribe, error) {
+	pbmsg := &pb.Subscription{}
+	if err := proto.Unmarshal(pktBytes, pbmsg); err != nil {
+		return nil, fmt.Errorf("Cannot unmarshal received packet. Closing connection: %v", err)
+	}
+
+	return &msgUnsubscribe{
+		VNI: pbmsg.Vni,
+	}, nil
+}
+
+func DeserializeUpdateMsg(pktBytes []byte) (*msgUpdate, error) {
+	pbmsg := &pb.Update{}
+	if err := proto.Unmarshal(pktBytes, pbmsg); err != nil {
+		return nil, fmt.Errorf("Cannot unmarshal received packet. Closing connection: %v", err)
+	}
+
+	action := ADD
+	if pbmsg.Action == pb.Action_REMOVE {
+		action = REMOVE
+	}
+
+	ipversion := IPV6
+	if pbmsg.Destination.IpVersion == pb.IPVersion_IPv4 {
+		ipversion = IPV4
+	}
+
+	destination := msgUpdateDestination{
+		IPVersion:    ipversion,
+		Prefix:       pbmsg.Destination.Prefix[:ipversion],
+		PrefixLength: uint8(pbmsg.Destination.PrefixLength),
+	}
+
+	nexthop := msgUpdateNextHop{
+		TargetAddress:    pbmsg.NextHop.TargetAddress[:ipversion],
+		TargetVNI:        pbmsg.NextHop.TargetVNI,
+		NAT:              pbmsg.NextHop.Nat,
+		NATPortRangeFrom: uint16(pbmsg.NextHop.NatPortRangeFrom),
+		NATPortRangeTo:   uint16(pbmsg.NextHop.NatPortRangeTo),
+	}
+
+	return &msgUpdate{
+		Action:      action,
+		VNI:         pbmsg.Vni,
+		Destination: destination,
+		NextHop:     nexthop,
+	}, nil
+}
